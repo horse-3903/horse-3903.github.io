b9126737471fbf2f0d5b155f022793a936b51317
Rafael Chong <86842464+horse-3903@users.noreply.github.com>
2025-10-19 02:33:10 +0800

    v0.0.6b: fixed language

--- src/i18n/languages/es.ts
+++ src/i18n/languages/es.ts
@@ -5,6 +5,7 @@ export const es: Translation = {
 	[Key.home]: "Inicio",
 	[Key.about]: "Sobre mí",
 	[Key.archive]: "Archivo",
+	[Key.blogs]: "Blogs",
 	[Key.search]: "Buscar",
 
 	[Key.tags]: "Etiquetas",
